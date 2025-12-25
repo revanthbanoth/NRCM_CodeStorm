375cf747221495620f112449d07d5bc3ddf0ffc2
Revanth <brevanth50@gmail.com>
2025-12-25 19:49:17 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,26 +4,21 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = process.env.DATABASE_URL
-    ? new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'mysql',
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'mysql',
+      logging: false
     })
-    : new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: process.env.DB_HOST,
-            dialect: 'mysql',
-            logging: false,
-        }
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false
+      }
     );
+
 
 const connectDB = async () => {
     try {
