7d0ca4263b334b22eca369d76eaee820f9446fa7
Revanth <brevanth50@gmail.com>
2025-12-30 10:48:40 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -11,19 +11,15 @@ const protect = (req, res, next) => {
     }
 
     if (!token) {
-        return res.status(401).json({
-            message: 'Not authorized, no token'
-        });
+        return res.status(401).json({ message: 'Not authorized, no token' });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // { id, role }
+        req.user = decoded;
         next();
     } catch (error) {
-        return res.status(401).json({
-            message: 'Not authorized, token failed'
-        });
+        return res.status(401).json({ message: 'Not authorized, token failed' });
     }
 };
 
