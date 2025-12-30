234d8f623ebab779873d4c9f9174955ec64da9a1
Revanth <brevanth50@gmail.com>
2025-12-30 12:38:59 +0530

    Update Idea.js

--- server/models/Idea.js
+++ server/models/Idea.js
@@ -1,51 +1,47 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/db');
 
-const Idea = sequelize.define(
-    'Idea',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        userId: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        },
-        teamName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        leaderEmail: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        projectTitle: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        theme: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        pptName: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        pptType: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        pptSize: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        }
-    },
-    {
-        tableName: 'Ideas',
-        timestamps: false
-    }
-);
+const Idea = sequelize.define('Idea', {
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  leaderEmail: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  projectTitle: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  theme: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  problemStatement: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  solutionDescription: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  techStack: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: 'Pending'
+  },
+  pptPath: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+});
 
 module.exports = Idea;
