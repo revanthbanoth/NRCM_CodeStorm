4ebb1710441f9891b642ad562beb352aa91e7021
Revanth <brevanth50@gmail.com>
2025-12-30 12:00:42 +0530

    Update Idea.js

--- server/models/Idea.js
+++ server/models/Idea.js
@@ -1,47 +1,51 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db');
+const sequelize = require('../config/db');
 
-const Idea = sequelize.define('Idea', {
-    userId: {
-        type: DataTypes.INTEGER,
-        allowNull: true
+const Idea = sequelize.define(
+    'Idea',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        userId: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        teamName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        leaderEmail: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        projectTitle: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        theme: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        pptName: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        pptType: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        pptSize: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        }
     },
-    teamName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    leaderEmail: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    projectTitle: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    theme: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    problemStatement: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    solutionDescription: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    techStack: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    status: {
-        type: DataTypes.STRING,
-        defaultValue: 'Pending'
-    },
-    pptPath: {
-        type: DataTypes.STRING,
-        allowNull: true
+    {
+        tableName: 'Ideas',   // ✅ EXACT DB TABLE NAME
+        timestamps: false     // ✅ VERY IMPORTANT
     }
-});
+);
 
 module.exports = Idea;
