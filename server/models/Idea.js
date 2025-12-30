71ea60081abc4d6c7516c71546a14d5839e59205
Revanth <brevanth50@gmail.com>
2025-12-30 13:08:17 +0530

    Update Idea.js

--- server/models/Idea.js
+++ server/models/Idea.js
@@ -1,59 +1,39 @@
+const sequelize = require('../config/db');
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db'); // ✅ MUST be destructured
 
 const Idea = sequelize.define('Idea', {
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: true
-  },
-
-  teamName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
-  leaderEmail: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
   projectTitle: {
     type: DataTypes.STRING,
     allowNull: false
   },
-
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
   theme: {
     type: DataTypes.STRING,
     allowNull: false
   },
-
   problemStatement: {
     type: DataTypes.TEXT,
     allowNull: false
   },
-
   solutionDescription: {
     type: DataTypes.TEXT,
     allowNull: false
   },
-
   techStack: {
     type: DataTypes.TEXT,
     allowNull: false
   },
-
   status: {
     type: DataTypes.STRING,
     defaultValue: 'Pending'
   },
-
   pptPath: {
     type: DataTypes.STRING,
     allowNull: true
   }
-}, {
-  tableName: 'Ideas',   // ✅ IMPORTANT (matches TiDB table)
-  timestamps: true
 });
 
 module.exports = Idea;
